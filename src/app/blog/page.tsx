30130c085bf7040c8c170107415155b6938a069b
ammaralizahid <amilzak42@gmail.com>
2026-02-09 16:32:14 +0500

    Blog Page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/layout";
+import Blog from "@/components/blog";
+
+export default function Blogs() {
+  return (
+    <Layout>
+      <Blog/>
+    </Layout>
+  );
+}
---
+++ src/app/faq/page.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/layout";
+import Faq from "@/components/faq";
+
+export default function Faqs() {
+  return (
+    <Layout>
+      <Faq/>
+    </Layout>
+  );
+}
